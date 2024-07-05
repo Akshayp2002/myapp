97d8336648dfceb1ab82a35afd2872614998ee63
Akshay <akshay@cloudrevelinnovation.com>
2024-07-05 18:24:22 +0530

    flowbite and dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login');
 });
+Route::get('/notes', function () {
+    return view('app.notes-view');
+})->name('notes');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
